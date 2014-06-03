3f491cdfb9ba94958575909d7875c6a23841242b
Constantin Gaul <Constantin.Gaul@gmail.com>
2014-06-03 18:45:23 +0200

    Further development on the RemoteDependencyAgent. Starts in offline context now and reverts its context to normal mode only, if all dependent ActorRefs are resolved. TODO: Test!

--- Code/Cloud-Federation/src/main/scala/agents/RemoteDependencyAgent.scala
+++ Code/Cloud-Federation/src/main/scala/agents/RemoteDependencyAgent.scala
@@ -7,22 +7,42 @@ import akka.actor.Identify
 /**
  * Created by costa on 6/3/14.
  */
-class RemoteDependencyAgent(remoteDependencies: Vector[ActorSelection]) extends Actor with ActorLogging
+abstract class RemoteDependencyAgent(remoteDependencies: Vector[ActorSelection]) extends Actor with ActorLogging
 {
+/* Values: */
+/* ======= */
+
 	//Initializes an internal boolean vector with false for every ActorSelection in remoteDependencies:
 	private val resolvedActors: ArrayBuffer[Option[ActorRef]] = ArrayBuffer.fill(remoteDependencies.length)(None)
 
 
-	def online: Receive = ???
-	def offline: Receive = ???
+/* Execution: */
+/* ======== */
 
-	def sendIdentityRequests(actors: List[ActorSelection]) = {
+	//RemoteDependencyAgent starts in offline context.
+	context.become(offline)
+	this.sendIdentityRequests(remoteDependencies)
+
+
+/* Methods: */
+/* ======== */
+
+	// Akka Actor Send & Receive method-handling:
+	// ------------------------------------------
+
+	def sendIdentityRequests(actors: Vector[ActorSelection]) = {
 
 		var actorID: Integer = 0
 		for (actor <- actors){
 			actor ! Identify(actorID)
 			actorID += 1
 		}
+	}
+
+
+	def offline: Receive = {
+		case ActorIdentity(actorID, actorRef)	=> recvActorIdentity(actorID, actorRef)
+		case _											=> log.error("Offline RemoteDependencyAgents should not receive anything!")
 	}
 
 	def recvActorIdentity(actorID: Any, actorRef: Option[ActorRef]): Unit = {
@@ -35,8 +55,8 @@ class RemoteDependencyAgent(remoteDependencies: Vector[ActorSelection]) extends 
 
 		//If no unresolved ActorRefs are left, become online:
 		if(unresolved == None){
-			context.become(online)
-			log.info("All ActorRef dependencies solved. Actor becomes ONLINE.")
+			context.unbecome()
+			log.info("All ActorRef dependencies solved. Actor is now ONLINE (using regular receive() method).")
 		}
 		//Else become offline (needed, as dependent Actors could be shutdown on runtime,
 		// which draws child actors offline again.
@@ -44,9 +64,5 @@ class RemoteDependencyAgent(remoteDependencies: Vector[ActorSelection]) extends 
 			context.become(offline)
 			log.info("Some ActorRef dependencies unsolved. Actor becomes OFFLINE.")
 		}
-	}
-
-	override def receive: Receive = {
-		case ActorIdentity(actorID, actorRef) => recvActorIdentity(actorID, actorRef)
 	}
 }
