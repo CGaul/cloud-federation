cba50c5e18783c50f2f537044dbbbfe20f9c5859
Constantin Gaul <Constantin.Gaul@gmail.com>
2014-09-20 18:07:09 +0200

    Changed start-script to only start one floodlight OFC with config1

--- Code/Network-Federation/Floodlight-Control/start_floodlight-OFC.sh
+++ Code/Network-Federation/Floodlight-Control/start_floodlight-OFC.sh
@@ -5,8 +5,8 @@ mkdir logs
 echo "Starting floodlight-controller-1..."
 java -jar floodlight.jar -cf openvirtex-defaultctl_1.floodlight > ./logs/ctrl1.log 2>&1 &
 
-echo "Starting floodlight-controller-2..."
-java -jar floodlight.jar -cf openvirtex-defaultctl_1.floodlight > ./logs/ctrl2.log 2>&1 &
+# echo "Starting floodlight-controller-2..."
+# java -jar floodlight.jar -cf openvirtex-defaultctl_1.floodlight > ./logs/ctrl2.log 2>&1 &
 
 exit 0
 
