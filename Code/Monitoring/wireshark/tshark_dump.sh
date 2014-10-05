ba1f6d388209c22cc6bf09a27a6ec47d7452fd65
Constantin Gaul <Constantin.Gaul@gmail.com>
2014-10-05 16:29:14 +0200

    Added "Monitoring" Coding-Dir. This includes the tmuxinator configs, wireshark dump scripts. More scripts on logfiles could also go in here.

---
+++ Code/Monitoring/wireshark/tshark_dump.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+
+######################################
+##         Global Constants:        ##
+######################################
+
+TIME_NOW=$(date +"%y-%m-%d_%H-%M-%S")
+CURR_USER=$(id -un)
+
+
+
+######################################
+##       Function Parameters:       ##
+######################################
+
+IFACE=$1
+
+
+
+######################################
+##        Program Execution:        ##
+######################################
+
+LOGFILE_DIR=~/logs/tshark
+LOGFILE_NAME="${TIME_NOW}.${IFACE}.$(hostname).pcap"
+
+## Program call-evaluation: ##
+##############################
+
+if ! [ "${CURR_USER}" = "root" ]; then
+	echo "Application must be run as root! Current user is \"${CURR_USER}\". Aborting script..."
+	exit 1
+fi
+
+if [[ ! -f ${LOGFILE_DIR} ]]; then
+	echo "Creating ${LOGFILE_DIR} directory..."
+	su -c "mkdir -p ${LOGFILE_DIR}" ${SUDO_USER}
+fi
+
+if [[ -x `which tshark` ]]; then
+	echo "Dumping with tshark on Interface ${IFACE} as root."
+	su -c "touch ${LOGFILE_DIR}/${LOGFILE_NAME}" ${SUDO_USER}
+	chmod o+rw ${LOGFILE_DIR}/${LOGFILE_NAME}
+	tshark -i ${IFACE} -w ${LOGFILE_DIR}/${LOGFILE_NAME}
+	exit 0
+else
+	echo "tshark not found!"
+	exit 2
+fi
