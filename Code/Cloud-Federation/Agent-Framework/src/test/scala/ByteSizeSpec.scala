82ab3d3b66efda34c1bb137330ef1b083d29a745
Constantin Gaul <Constantin.Gaul@gmail.com>
2014-10-20 20:36:58 +0200

    Small appends to the ByteSizeSpec.
    CITMASTER-28 - Develop NetworkResourceAgent

--- Code/Cloud-Federation/Agent-Framework/src/test/scala/ByteSizeSpec.scala
+++ Code/Cloud-Federation/Agent-Framework/src/test/scala/ByteSizeSpec.scala
@@ -1,21 +1,28 @@
 import datatypes.{Byte_Unit, ByteSize}
-import org.scalatest.{Matchers, FlatSpec}
+import org.scalatest.{ShouldMatchers, FlatSpec}
 
 /**
  * Created by costa on 10/20/14.
  */
-class ByteSizeSpec extends FlatSpec with Matchers{
-	"A ByteSize Value" should "be equal and comparable to another Value with the same size and unit" in{
+class ByteSizeSpec extends FlatSpec with ShouldMatchers{
+	"A ByteSize Value" should "be equal and comparable to itself" in{
 		val byteSize1 = new ByteSize(5, Byte_Unit.MB)
-		val byteSize2 = new ByteSize(5, Byte_Unit.MB)
-
-		byteSize1.equals(byteSize2) should be (true)
-		byteSize2.equals(byteSize1) should be (true)
-		byteSize1.compareTo(byteSize2) should be (0)
-		byteSize2.compareTo(byteSize1) should be (0)
+		byteSize1.equals(byteSize1) should be (true)
+		byteSize1.compareTo(byteSize1) should be (0)
 	}
 
-	it should "be equal to another Value's size calculated in Bytes, using Binary IEC Metric (KiB, MiB, GiB)" in {
+	it should "be equal and comparable to another Value with the same size and unit" in{
+	  val byteSize1 = new ByteSize(5, Byte_Unit.MB)
+	  val byteSize2 = new ByteSize(5, Byte_Unit.MB)
+
+	  byteSize1.equals(byteSize2) should be (true)
+	  byteSize2.equals(byteSize1) should be (true)
+	  byteSize1.compareTo(byteSize2) should be (0)
+	  byteSize2.compareTo(byteSize1) should be (0)
+	  }
+
+	it should "be equal and comparable to another Value's size calculated in Bytes, " +
+	  "using Binary IEC Metric (KiB, MiB, GiB) - used in RAM and OS internal calculation" in {
 		val byteSize1 = new ByteSize(0.09765625, Byte_Unit.GiB)
 		val byteSize2 = new ByteSize(100, Byte_Unit.MiB)
 		val byteSize3 = new ByteSize(102400, Byte_Unit.KiB)
@@ -36,7 +43,8 @@ class ByteSizeSpec extends FlatSpec with Matchers{
 		byteSize3.compareTo(byteSize2) should be (0)
 	}
 
-	it should "be equal to another Value's size calculated in Bytes, using Decimal Metric (kB, MB, GB)" in {
+	it should "be equal and comparable to another Value's size calculated in Bytes, " +
+	  "using Decimal Metric (kB, MB, GB) - used in regular HDD calculation" in {
 		val byteSize1 = new ByteSize(1, Byte_Unit.GB)
 		val byteSize2 = new ByteSize(1000, Byte_Unit.MB)
 		val byteSize3 = new ByteSize(1000000, Byte_Unit.kB)
@@ -57,12 +65,16 @@ class ByteSizeSpec extends FlatSpec with Matchers{
 		byteSize3.compareTo(byteSize2) should be (0)
 	}
 
-	it should "be comparable between Decimal and Binary IEC Metric (KB vs. KiB, MB vs. MiB, GB vs. GiB)" in {
-		val byteSize1 = new ByteSize(100, Byte_Unit.MiB)
-		val byteSize2 = new ByteSize(104.85759999999999, Byte_Unit.MB)
+	it should "be equal and comparable between " +
+	  "Decimal and Binary IEC Metric (KB vs. KiB, MB vs. MiB, GB vs. GiB)" in {
+		val byteSize1 = new ByteSize(1000, Byte_Unit.KiB)
+		val byteSize2 = new ByteSize(1024, Byte_Unit.kB)
 
-		val byteSize3 = new ByteSize(100, Byte_Unit.GiB)
-		val byteSize4 = new ByteSize(107.37418240000001, Byte_Unit.GB)
+		val byteSize3 = new ByteSize(100, Byte_Unit.MiB)
+		val byteSize4 = new ByteSize(104.85759999999999, Byte_Unit.MB)
+
+		val byteSize5 = new ByteSize(100, Byte_Unit.GiB)
+		val byteSize6 = new ByteSize(107.37418240000001, Byte_Unit.GB)
 
 		byteSize1.equals(byteSize2) should be (true)
 		byteSize2.equals(byteSize1) should be (true)
@@ -73,5 +85,10 @@ class ByteSizeSpec extends FlatSpec with Matchers{
 		byteSize4.equals(byteSize3) should be (true)
 		byteSize3.compareTo(byteSize4) should be (0)
 		byteSize4.compareTo(byteSize3) should be (0)
+
+		byteSize5.equals(byteSize6) should be (true)
+		byteSize6.equals(byteSize5) should be (true)
+		byteSize5.compareTo(byteSize6) should be (0)
+		byteSize6.compareTo(byteSize5) should be (0)
 	}
 }
