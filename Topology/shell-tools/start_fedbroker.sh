37544897dba8b3a82422d00788840d89ae771ec5
Constantin Gaul <Constantin.Gaul@gmail.com>
2015-02-08 21:53:47 +0000

    Added start_fedbroker.sh to Topology shell-tools.

---
+++ Topology/shell-tools/start_fedbroker.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+JARNAME="pubsub-system_0.3-SNAPSHOT.fat.jar"
+
+NOW=$(date +"%y-%m-%d_%H-%M-%S")
+LOGFILE=logs/fedbroker/${NOW}.log
+
+
+#Redirect a copy of stdout and errout into a logfile files:
+#(from: http://stackoverflow.com/questions/3173131/redirect-copy-of-stdout-to-log-file-from-within-bash-script-itself)
+exec >  >(tee -a ${LOGFILE})
+exec 2> >(tee -a ${LOGFILE} >&2)
+
+echo "Starting PubSub-System at time ${NOW}..." 
+wget --auth-no-challenge --user='citcloud' --password='fneA48pcEno9WiW1nYipdoPQiY367ZwbIc4' -N http://lab.is-by.us:8080/job/Cloud-Federation/lastSuccessfulBuild/artifact/Cloud-Federation/PubSub-System/target/scala-2.11/${JARNAME} -P ~/Development/
+
+
+java -jar ~/Development/${JARNAME} --appconf ~/Development/federator_application.conf --fedconf ~/Development/fedconf
