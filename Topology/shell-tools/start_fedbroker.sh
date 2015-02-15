2056a91246bb6d71ffcd2e54cc863346ee0b39ba
Constantin Gaul <Constantin.Gaul@gmail.com>
2015-02-15 22:28:25 +0000

    Adjusted start_fedbroker to renamed PubSub-System, that is now called Federation-Broker.

--- Topology/shell-tools/start_fedbroker.sh
+++ Topology/shell-tools/start_fedbroker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-JARNAME="pubsub-system_0.3-SNAPSHOT.fat.jar"
+JARNAME="fed-broker_0.3-SNAPSHOT.fat.jar"
 
 NOW=$(date +"%y-%m-%d_%H-%M-%S")
 LOGFILE=logs/fedbroker/${NOW}.log
@@ -11,8 +11,8 @@ LOGFILE=logs/fedbroker/${NOW}.log
 exec >  >(tee -a ${LOGFILE})
 exec 2> >(tee -a ${LOGFILE} >&2)
 
-echo "Starting PubSub-System at time ${NOW}..." 
-wget --auth-no-challenge --user='citcloud' --password='fneA48pcEno9WiW1nYipdoPQiY367ZwbIc4' -N http://lab.is-by.us:8080/job/Cloud-Federation/lastSuccessfulBuild/artifact/Cloud-Federation/PubSub-System/target/scala-2.11/${JARNAME} -P ~/Development/
+echo "Starting Federation-Broker at time ${NOW}..." 
+wget --auth-no-challenge --user='citcloud' --password='fneA48pcEno9WiW1nYipdoPQiY367ZwbIc4' -N http://lab.is-by.us:8080/job/Cloud-Federation/lastSuccessfulBuild/artifact/Cloud-Federation/Federation-Broker/target/scala-2.11/${JARNAME} -P ~/Development/
 
 
-java -jar ~/Development/${JARNAME} --appconf ~/Development/federator_application.conf --fedconf ~/Development/fedconf
+java -jar ~/Development/${JARNAME} --appconf ~/Development/federator_application.conf --fedconf ~/Development/federatorconf
